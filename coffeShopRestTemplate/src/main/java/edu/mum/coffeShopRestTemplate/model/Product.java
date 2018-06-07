68cf68b89fcf81972e982c1317de8e6a1b8d76f5
jmjjaya <efacjeewan@gmail.com>
2018-06-07 16:38:16 -0500

    Implemented Product test create

---
+++ coffeShopRestTemplate/src/main/java/edu/mum/coffeShopRestTemplate/model/Product.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package edu.mum.coffeShopRestTemplate.model;
+
+/**
+ * @author jeewa
+ *
+ */
+public class Product {
+
+	private int id;
+	private String productName;
+	private String description;
+	private double price;
+	private ProductType productType;
+
+	public Product() {
+		super(); // default constructor
+	}
+
+	public Product(String productName, String description, double price, ProductType productType) {
+		super();
+		this.productName = productName;
+		this.description = description;
+		this.price = price;
+		this.productType = productType;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public ProductType getProductType() {
+		return productType;
+	}
+
+	public void setProductType(ProductType productType) {
+		this.productType = productType;
+	}
+
+}
