2933a14496b9673e9680e7889c750cc9313c08e5
jmjjaya <efacjeewan@gmail.com>
2018-06-08 13:33:27 -0500

    Implemented Product add update view and delete views

--- coffeeShopMVC/src/main/java/edu/mum/coffee/controller/ProductController.java
+++ coffeeShopMVC/src/main/java/edu/mum/coffee/controller/ProductController.java
@@ -3,67 +3,76 @@
  */
 package edu.mum.coffee.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import edu.mum.coffee.domain.Product;
+import edu.mum.coffee.domain.ProductType;
 import edu.mum.coffee.service.ProductService;
 
 /**
  * @author jeewa
  *
  */
-@RestController()
+@Controller()
+@RequestMapping("/product")
 public class ProductController {
-	
+
 	@Autowired
 	ProductService productService;
-	
-	@RequestMapping(value = "/product/create", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
 	public String getCreateProduct(Model model) {
 		Product product = new Product();
-		model.addAttribute("txt_submit_btn", "Create");
 		model.addAttribute("product", product);
-		return "creaOrEdittOrder";
+		
+		return "createOrEditProduct";
 	}
-	
-	@RequestMapping(value = "/product/edit/{id}", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
 	public String getEditProduct(@PathVariable("id") int id, Model model) {
 		Product product = productService.getProduct(id);
-		model.addAttribute("txt_submit_btn", "Update");
 		model.addAttribute("product", product);
-		return "creaOrEdittOrder";
+		
+		return "createOrEditProduct";
 	}
-	
-	@RequestMapping(value = "/product/createOrEdit", method = RequestMethod.POST)
-	public String createProduct(@RequestBody Product product, Model model) {
+
+	@RequestMapping(value = {"/createOrEdit"}, method = RequestMethod.POST)
+	public String createProduct(@ModelAttribute Product product, Model model) {
+		System.out.println(product.getDescription());
 		productService.save(product);
-		return "redirect:/product/edit/"+ product.getId();
+		return "redirect:/product/list";
 	}
-	
-	/*@RequestMapping(value = "/product/edit", method = RequestMethod.PUT)
-	public Product editProduct(@RequestBody Product product) {
-		productService.save(product);
-		return product;
-	}*/
-	
-	@GetMapping("product/list")
+
+	@ModelAttribute
+	public void addAttributes(Model model) {
+		List <ProductType> productTypes = new ArrayList<ProductType> (Arrays.asList(ProductType.values()));
+        model.addAttribute("productTypes", productTypes);
+	}
+	/*
+	 * @RequestMapping(value = "/product/edit", method = RequestMethod.PUT) public
+	 * Product editProduct(@RequestBody Product product) {
+	 * productService.save(product); return product; }
+	 */
+
+	@GetMapping(value = {"/list"})
 	public String listAllProduct(Model model) {
-		List<Product> products =  productService.getAllProduct();
+		List<Product> products = productService.getAllProduct();
 		model.addAttribute("products", products);
 		return "products";
 	}
-	
-	@DeleteMapping("product/remove/{id}")
+
+	@RequestMapping(value = "/remove/{id}" , method = RequestMethod.GET)
 	public String reomoveProduct(@PathVariable("id") int id) {
 		Product product = productService.getProduct(id);
 		productService.delete(product);
